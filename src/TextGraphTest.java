573ccb133d802c21e288ac4434da2a2a85f60584
Red-ALGA <2243481865@qq.com>
2025-05-28 15:51:57 +0800

    白盒测试

--- src/TextGraphTest.java
+++ src/TextGraphTest.java
@@ -2,87 +2,59 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-//黑盒测试
+//白盒测试
 public class TextGraphTest {
     private TextGraph.Graph graph;
 
     @Before
     public void setUp() {
-        //初始化测试所需的图结构——根据设计测试用例时编写的测试文件创建
+        //构建测试图用例
         graph = new TextGraph.Graph(true);
-
-        //1-3,6,8的图结构
-        graph.addEdge("a", "b", 1);
-        graph.addEdge("b", "c", 1);
-        graph.addEdge("c", "d", 1);
-
-        //TC3的图结构
-        graph.addEdge("f", "g", 1);
-        graph.addEdge("g", "h", 1);
-        graph.addEdge("c", "d", 1);
-
-        //TC5,7的图结构
-        graph.addEdge("j", "k1", 1);
-        graph.addEdge("j", "k2", 1);
-        graph.addEdge("k1", "l", 1);
-        graph.addEdge("k2", "l", 1);
-
+        graph.addEdge("hot", "warm", 1);
+        graph.addEdge("warm", "dog", 1);
+        graph.addEdge("hot", "spicy", 1);
+        graph.addEdge("spicy", "dog", 1);
+        graph.addEdge("apple", "boy", 1);
+        graph.addEdge("boy", "cat", 1);
+        graph.addEdge("hello", "world", 1);
     }
 
     @Test
-    public void testT1()
-    {   //正常存在一个桥接词
-        String result = TextGraph.generateNewText(graph, "a c d");
-        assertEquals("a b c d", result);
+    public void testWord1NotInGraph()
+    {   //第一个输入不在有向图中
+        String result = TextGraph.queryBridgeWords(graph, "nonexist", "hello");
+        assertEquals("No nonexist in the graph!", result);
     }
 
     @Test
-    public void testT2()
-    {   //输入为空
-        String result = TextGraph.generateNewText(graph, "");
-        assertEquals("", result);
+    public void testWord2NotInGraph()
+    {   //第二个输入不在有向图中
+        String result = TextGraph.queryBridgeWords(graph, "hello", "nonexist");
+        assertEquals("No nonexist in the graph!", result);
     }
 
     @Test
-    public void testT3()
-    {   //输入为空
-        String result = TextGraph.generateNewText(graph, "e");
-        assertEquals("e", result);
+    public void testNoBridgeWords()
+    {   //两个单词之间没有桥接词
+        String result = TextGraph.queryBridgeWords(graph, "hello", "world");
+        assertEquals("No bridge words from hello to world!", result);
     }
 
     @Test
-    public void testT4()
-    {   //存在单个桥接词，有特殊字符
-        String result = TextGraph.generateNewText(graph, "f! h*");
-        assertEquals("f g h", result);
+    public void testSingleBridgeWord()
+    {   //两个单词只有一个桥接词
+
+        String result = TextGraph.queryBridgeWords(graph, "apple", "cat");
+        assertEquals("The bridge word from apple to cat is: boy", result);
     }
 
     @Test
-    public void testT5()
-    {   //有多个桥接词
-        String result = TextGraph.generateNewText(graph, "j l");
-        assertTrue(result.equals("j k1 l") || result.equals("j k2 l"));
-    }
+    public void testMultipleBridgeWords()
+    {   //两个单词有多个桥接词
+        String result = TextGraph.queryBridgeWords(graph, "hot", "dog");
 
-    @Test
-    public void testT6()
-    {   //无桥接词
-        String result = TextGraph.generateNewText(graph, "a b c");
-        assertEquals("a b c", result);
+        assertTrue(result.startsWith("The bridge words from hot to dog are: "));
+        assertTrue(result.contains("warm"));
+        assertTrue(result.contains("spicy"));
     }
-
-    @Test
-    public void testT7()
-    {   //多个桥接词+特殊字符
-        String result = TextGraph.generateNewText(graph, "j! l*");
-        assertTrue(result.equals("j k1 l") || result.equals("j k2 l"));
-    }
-
-    @Test
-    public void testT8()
-    {   //判断大小写敏感
-        String result = TextGraph.generateNewText(graph, "a! b*");
-        assertEquals("a b", result);
-    }
-
 }
